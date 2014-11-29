2250d4c99dd887a748ddb20536f2fcdb45890795
Bilal Çınarlı <bcinarli@gmail.com>
2014-11-29 20:45:28 +0200

    Update md.php
    Some overwrites on table methods

--- misto/md.php
+++ misto/md.php
@@ -11,7 +11,7 @@ use \Michelf\MarkdownExtra;
  * @dependency Michelf/Markdown
  * @dependency Michelf/MarkdownExtra
  */
-class md
+class md extends MarkdownExtra
 {
 	private static $_valid_file = false;
 	private static $_file;
@@ -19,6 +19,7 @@ class md
 	private static $_meta = array();
 	private static $_content;
 	public static $html;
+    public static $doc_root = '_articles';
 
 	/**
 	 * constructor function for Markdown parser in Misto.
@@ -26,6 +27,8 @@ class md
 	 */
 	public function __construct($file)
 	{
+        parent::__construct();
+
 		self::$_file = $file;
 
 		// check if developer added the md file ext. while calling the constructor
@@ -33,12 +36,12 @@ class md
 			self::$_file = $file . '.md';
 		}
 
-		self::is_exists();
+		$this->is_exists();
 
 		if (self::$_valid_file === true) {
-			self::getMetadata();
-			self::setMetadata();
-			self::transformMD();
+			$this->getMetadata();
+			$this->setMetadata();
+		    $this->transformMD();
 		}
 	}
 
@@ -47,13 +50,13 @@ class md
 	 */
 	private function is_exists()
 	{
-		$file_path = ABS_PATH . APP_PATH . self::$_file;
+		$file_path = ABS_PATH . APP_PATH . self::$doc_root . '/' . self::$_file;
 
 		// check if file exists
 		if (!file_exists($file_path)) {
 			// article names starts with their dates
-			if (strstr($file_path, '_articles')) {
-				$file_path = str_replace('_articles/', '_articles/*', $file_path);
+			if (strstr($file_path, self::$doc_root)) {
+				$file_path = str_replace(self::$doc_root . '/', self::$doc_root . '/*', $file_path);
 				$article   = glob($file_path);
 
 				if (!$article) {
@@ -90,6 +93,10 @@ class md
 
 			return;
 		}
+
+        if(!isset($tmp[1])){
+            return;
+        }
 
 		$tmp_meta = explode("\n", $tmp[1]);
 
@@ -154,6 +161,228 @@ class md
 		}
 	}
 
+    /**
+     * Inline code overwrites for class/id attribute support
+     */
+    protected function makeCodeSpan($code){
+        $code = preg_replace_callback('{
+                ([\*-_:a-zA-Z0-9 ]+)
+                ('.$this->id_class_attr_catch_re.' )?
+                }mx',
+            array(&$this, '_makeCodeSpan_callback'), $code);
+
+        return $code;
+    }
+
+    protected function _makeCodeSpan_callback($matches){
+        $code = trim($matches[1]);
+        $attrs =& $matches[3];
+
+        $attr_str = $this->doExtraAttributes($this->code_attr_on_pre ? "pre" : "code", $attrs);
+
+        return '<code' . $attr_str . '>' . $code . '</code>';
+    }
+
+    protected function doTables($text) {
+        #
+        # Form HTML tables.
+        #
+        $less_than_tab = $this->tab_width - 1;
+        #
+        # Find tables with leading pipe.
+        #
+        #	| Header 1 | Header 2
+        #	| -------- | --------
+        #	| Cell 1   | Cell 2
+        #	| Cell 3   | Cell 4
+        #
+        $text = preg_replace_callback('
+			{
+				^							# Start of a line
+				[ ]{0,'.$less_than_tab.'}	# Allowed whitespace.
+				[|]							# Optional leading pipe (present)
+				(.+) \n						# $1: Header row (at least one pipe)
+
+				[ ]{0,'.$less_than_tab.'}	# Allowed whitespace.
+				[|] ([ ]*[-:]+[-| :]*) \n	# $2: Header underline
+
+				(							# $3: Cells
+					(?>
+						[ ]*				# Allowed whitespace.
+						[|] .* \n			# Row content.
+					)*
+				)
+				(?=\n|\Z)					# Stop at final double newline.
+			}xm',
+            array(&$this, '_doTable_leadingPipe_callback'), $text);
+
+        #
+        # Find tables without header.
+        #   !---     | ---
+        #	Cell 1   | Cell 2
+        #	Cell 3   | Cell 4
+        #
+        $text = preg_replace_callback('
+			{
+				^							# Start of a line
+				[!] ([ ]*[-:]+[-| :]*) \n	# $2: Header underline
+				(							# $3: Cells
+					(?>
+						.* [|] .* \n		# Row content
+					)*
+				)
+				(?=\n|\Z)					# Stop at final double newline.
+			}xm',
+            array(&$this, '_DoTable_withoutHeader_callback'), $text);
+
+
+        #
+        # Find tables without leading pipe.
+        #
+        #	Header 1 | Header 2
+        #	-------- | --------
+        #	Cell 1   | Cell 2
+        #	Cell 3   | Cell 4
+        #
+        $text = preg_replace_callback('
+			{
+				^							# Start of a line
+				[ ]{0,'.$less_than_tab.'}	# Allowed whitespace.
+				(\S.*[|].*) \n				# $1: Header row (at least one pipe)
+
+				[ ]{0,'.$less_than_tab.'}	# Allowed whitespace.
+				([-:]+[ ]*[|][-| :]*) \n	# $2: Header underline
+
+				(							# $3: Cells
+					(?>
+						.* [|] .* \n		# Row content
+					)*
+				)
+				(?=\n|\Z)					# Stop at final double newline.
+			}xm',
+            array(&$this, '_DoTable_callback'), $text);
+
+        return $text;
+    }
+
+    protected function _doTable_withoutHeader_callback($matches){
+        $head = array();
+        $underline = $matches[1];
+        $content = $matches[2];
+
+        $content	= preg_replace('/^ *[|]/m', '', $content);
+
+        return $this->_doTable_callback(array($matches[0], $head, $underline, $content));
+    }
+
+    protected function _doTable_callback($matches) {
+        $head		= $matches[1];
+        $underline	= $matches[2];
+        $content	= $matches[3];
+
+
+        # Remove any tailing pipes for each line.
+        $head		= preg_replace('/[|] *$/m', '', $head);
+        $underline	= preg_replace('/[|] *$/m', '', $underline);
+        $content	= preg_replace('/[|] *$/m', '', $content);
+
+        # Reading alignement from header underline.
+        $separators	= preg_split('/ *[|] */', $underline);
+        $col_count = count($separators);
+        foreach ($separators as $n => $s) {
+            if (preg_match('/^ *-+: *$/', $s))
+                $attr[$n] = $this->_doTable_makeAlignAttr('right');
+            else if (preg_match('/^ *:-+: *$/', $s))
+                $attr[$n] = $this->_doTable_makeAlignAttr('center');
+            else if (preg_match('/^ *:-+ *$/', $s))
+                $attr[$n] = $this->_doTable_makeAlignAttr('left');
+            else
+                $attr[$n] = '';
+        }
+
+        # Parsing span elements, including code spans, character escapes,
+        # and inline HTML tags, so that pipes inside those gets ignored.
+        if(!empty($head)){
+            $head		= $this->parseSpan($head);
+            $headers	= preg_split('/ *[|] */', $head);
+            $attr       = array_pad($attr, $col_count, '');
+        }
+
+        # Write column headers.
+        $text = "<table>\n";
+        if(!empty($head)){
+            $text .= "<thead>\n";
+            $text .= "<tr>\n";
+            foreach ($headers as $n => $header)
+                $text .= $this->_doTable_cell(trim($header), $attr[$n], 'th') . "\n";
+            $text .= "</tr>\n";
+            $text .= "</thead>\n";
+        }
+
+        # Split content by row.
+        $rows = explode("\n", trim($content, "\n"));
+
+        $text .= "<tbody>\n";
+        foreach ($rows as $row) {
+            # Parsing span elements, including code spans, character escapes,
+            # and inline HTML tags, so that pipes inside those gets ignored.
+            $row = $this->parseSpan($row);
+
+            # Split row by cell.
+            $row_cells = preg_split('/ *[|] */', $row, $col_count);
+            $row_cells = array_pad($row_cells, $col_count, '');
+
+            $text .= "<tr>\n";
+            foreach ($row_cells as $n => $cell)
+                $text .= $this->_doTable_cell(trim($cell), $attr[$n]) . "\n";
+            $text .= "</tr>\n";
+        }
+        $text .= "</tbody>\n";
+        $text .= "</table>";
+
+        return $this->hashBlock($text) . "\n";
+    }
+
+    protected function _doTable_cell($content, $attr, $cell = 'td'){
+        $attr_str = '';
+
+        preg_match(
+                '{
+                    ('.$this->id_class_attr_catch_re.')
+                }xm', $content, $matches);
+
+        if(!empty($matches)){
+            $content = str_replace($matches[0], '', $content);
+            $attr_str = $this->doExtraAttributes($this->code_attr_on_pre ? "pre" : "code", $matches[2]);
+        }
+
+        return '<' . $cell . $attr . $attr_str . '>' . $this->runSpanGamut(trim($content)) . '</' . $cell . '>';
+    }
+
+    /**
+     * Builds the content
+     */
+    private function transformMD()
+    {
+        $this->code_attr_on_pre = true;
+        self::$html = $this->transform(self::$_content);
+    }
+
+    /**
+     * Corrects internal urls
+     */
+    public static function setLinks(){
+        $url_prefix = url::homepage() . '/';
+
+        // corrects anchors
+        self::$html = str_replace('href="/', 'href="' . $url_prefix, self::$html);
+
+        // corrects src paths
+        self::$html = str_replace('src="/', 'src="' . $url_prefix, self::$html);
+
+        return self::$html;
+    }
+
 	/**
 	 * Gets a single meta
 	 * @param $meta
@@ -163,14 +392,6 @@ class md
 		if (array_key_exists($meta, self::$_meta)) {
 			return self::$_meta[$meta];
 		}
-	}
-
-	/**
-	 * Builds the content
-	 */
-	private static function transformMD()
-	{
-		self::$html = MarkdownExtra::defaultTransform(self::$_content);
 	}
 
 	public static function getContent()
