2a3cd1ad2274d7997b5209e6a15c0ca6d3fff2c5
Bilal Çınarlı <bcinarli@gmail.com>
2015-05-14 10:29:47 +0300

    Change: router::setRoute method now updates the role

--- misto/router.php
+++ misto/router.php
@@ -6,128 +6,132 @@
  **/
 class router
 {
-	private static $_routes;
-	private static $_routeMatch = false;
-	private static $_page;
-	private static $_matches;
-	private static $_role = '404';
-	public static $is_404 = true;
+    private static $_routes;
+    private static $_routeMatch = false;
+    private static $_page;
+    private static $_matches;
+    private static $_role = '404';
+    public static $is_404 = true;
 
-	public function __construct()
-	{
-		require_once(ABS_PATH . APP_PATH . 'settings/routes.php');
+    public function __construct()
+    {
+        require_once(ABS_PATH . APP_PATH . 'settings/routes.php');
 
-		self::$_page = $routes['404']['page'];
-		$this->setRoutes($routes);
-	}
+        self::$_page = $routes['404']['page'];
+        $this->setRoutes($routes);
+    }
 
-	public function setRoutes(array $routes)
-	{
-		if (is_array($routes)) {
-			self::$_routes = $routes;
-			$this->analyseRoutes();
-		}
-	}
+    public function setRoutes(array $routes)
+    {
+        if (is_array($routes)) {
+            self::$_routes = $routes;
+            $this->analyseRoutes();
+        }
+    }
 
-	public function analyseRoutes()
-	{
-		foreach (self::$_routes as $route) {
-			$match = 'plain';
-			if (isset($route['match'])) {
-				$match = $route['match'];
-			}
+    public function analyseRoutes()
+    {
+        foreach (self::$_routes as $route) {
+            $match = 'plain';
+            if (isset($route['match'])) {
+                $match = $route['match'];
+            }
 
-			switch ($match) {
-				case 'plain':
-				default:
-					$this->checkPlainRoute($route['url']);
-					break;
-				case 'regex':
-					$this->checkRegexRoute($route['url']);
-					break;
-			}
+            switch ($match) {
+                case 'plain':
+                default:
+                    $this->checkPlainRoute($route['url']);
+                    break;
+                case 'regex':
+                    $this->checkRegexRoute($route['url']);
+                    break;
+            }
 
-			if (self::$_routeMatch === true) {
-				self::$is_404 = false;
-				$this->setRole($route);
-				$this->setPage($route);
-				break;
-			}
-		}
-	}
+            if (self::$_routeMatch === true) {
+                self::$is_404 = false;
+                self::setRole($route);
+                $this->setPage($route);
+                break;
+            }
+        }
+    }
 
-	private function setPage($route)
-	{
-		$page = $route['page'];
+    private function setPage($route)
+    {
+        $page = $route['page'];
 
-		if (isset($route['match']) && $route['match'] == 'regex') {
-			$matches = self::getMatches();
+        if (isset($route['match']) && $route['match'] == 'regex') {
+            $matches = self::getMatches();
 
-			// loop matches and replace $1, $2 like placeholders with matched vars
-			foreach ($matches as $key => $value) {
-				$page = str_replace('$' . $key, $value, $page);
-			}
+            // loop matches and replace $1, $2 like placeholders with matched vars
+            foreach ($matches as $key => $value) {
+                $page = str_replace('$' . $key, $value, $page);
+            }
 
-			// if url matched, and page if is not exists, set page to 404
-			if (!file_exists(APP_PATH . $page)) {
-				$page         = $this->_routes[404]['page'];
-				self::$is_404 = true;
-			}
-		}
+            // if url matched, and page if is not exists, set page to 404
+            if (!file_exists(APP_PATH . $page)) {
+                $page         = $this->_routes[404]['page'];
+                self::$is_404 = true;
+            }
+        }
 
-		return self::$_page = $page;
-	}
+        return self::$_page = $page;
+    }
 
-	private function setRole($route)
-	{
-		if (!isset($route['role'])) {
-			self::$_role = 'role';
-		} else {
-			self::$_role = $route['role'];
-		}
-	}
+    private static function setRole($route)
+    {
+        if (!isset($route['role'])) {
+            self::$_role = 'page';
+        } else {
+            self::$_role = $route['role'];
+        }
+    }
 
-	private function checkPlainRoute($url)
-	{
-		self::$_routeMatch = false;
-		if ($url == url::getUrl()) {
-			self::$_routeMatch = true;
-		}
-	}
+    private function checkPlainRoute($url)
+    {
+        self::$_routeMatch = false;
+        if ($url == url::getUrl()) {
+            self::$_routeMatch = true;
+        }
+    }
 
-	private function checkRegexRoute($url)
-	{
-		self::$_routeMatch = false;
+    private function checkRegexRoute($url)
+    {
+        self::$_routeMatch = false;
 
-		if (preg_match('#' . $url . '#', url::getUrl(), $matches)) {
-			self::$_matches    = $matches;
-			self::$_routeMatch = true;
-		}
-	}
+        if (preg_match('#' . $url . '#', url::getUrl(), $matches)) {
+            self::$_matches    = $matches;
+            self::$_routeMatch = true;
+        }
+    }
 
-	public static function setRoute($route)
-	{
-		if (array_key_exists('page', self::$_routes[$route])) {
-			return self::$_page = self::$_routes[$route]['page'];
-		}
-	}
+    public static function setRoute($route)
+    {
+        if (array_key_exists('page', self::$_routes[$route])) {
+            if (self::$_routes[$route]['role'] != self::getRole()) {
+                self::setRole(self::$_routes[$route]);
+            }
 
-	public static function getRoute()
-	{
-		return self::$_page;
-	}
+            return self::$_page = self::$_routes[$route]['page'];
+        }
+    }
 
-	public static function getRole()
-	{
-		return self::$_role;
-	}
+    public static function getRoute()
+    {
+        return self::$_page;
+    }
 
-	public static function getMatches($matched = null)
-	{
-		if ($matched == null) {
-			return self::$_matches;
-		}
+    public static function getRole()
+    {
+        return self::$_role;
+    }
 
-		return self::$_matches[$matched];
-	}
+    public static function getMatches($matched = null)
+    {
+        if ($matched == null) {
+            return self::$_matches;
+        }
+
+        return self::$_matches[$matched];
+    }
 }
